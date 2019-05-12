95f49c93999e58d22265674d4063b572d64b8337
Sowayi <sapwao@gmail.com>
2019-05-11 21:36:17 -0400

    Add display_all test case to test if users can view account details

--- testUser.py
+++ testUser.py
@@ -46,4 +46,15 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.users), 2)
 
+    def test_display_all(self):
+        """
+        test_display_all test case to test whether users can view their account details
+        """
+        self.new_user.save_user()
+        self.assertEqual(User.display_all(), User.users)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
     
