040fdaf40bc5583b65ada0b6c9c54f5b94a7354e
Sowayi <sapwao@gmail.com>
2019-05-11 21:58:44 -0400

    Add setup and teardown methods to run before and clean up after tests

--- testCredential.py
+++ testCredential.py
@@ -2,4 +2,24 @@ import unittest     #importing unittest module
 from credential import Credential   # importing class Credential
 import pyperclip    # importing pyperclip module
 
+class TestCredential(unittest.TestCase):
+    """
+    Test class that defines the test cases for the credential class behaviours
 
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+    def setUp(self):
+        """
+        Set up method to run before each test case.
+        """
+        self.new_credential = Credential("Sowasse","Pinterest", "sow@yi")
+
+    def tearDown(self):
+        """
+        Tear down method that cleans up after each test case has run
+        """  
+        Credential.credentials = []
+        
+          
